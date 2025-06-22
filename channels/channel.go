1a51ae379c5cb4138fd2484f28f9bbe0a069c1fa
smitambalia <smit.ambalia@gmail.com>
2025-06-22 17:31:50 +0530

    Add task and email sending functions to demonstrate goroutine usage and channel communication

--- channels/channel.go
+++ channels/channel.go
@@ -19,6 +19,21 @@ func sumOfTwo(resultChan chan int, num1 int, num2 int) {
 	sum := num1 + num2 // Calculate the sum of two numbers
 	resultChan <- sum // Send the result to the channel
 }
+func task(done chan bool) {
+	fmt.Println("Task Started")
+
+	done <- true // Signal that the task is done
+	fmt.Println("Task Completed")
+}
+
+func sendEmail(emailChan chan string, done chan bool) {
+	defer func() { done <- true }() // Signal that the email sending is done
+	for email := range emailChan {
+		fmt.Println("Sending email to:", email) // Simulate sending an email
+  		time.Sleep(time.Second) // Simulate some delay in sending the email
+ 	}
+}
+
 func main ( ) {
 	fmt.Println("Channels in Go")
 	// Channels are used to communicate between goroutines.
@@ -49,6 +64,41 @@ func main ( ) {
 	sum := <-result
 
 	fmt.Println("Sum of two numbers:", sum) // Receive the result from the channel
+
+	chan1 := make(chan int)
+	chan2 := make(chan string)
+	
+	go func() {
+		chan1 <- 23
+	} ()
+
+	go func() {
+		chan2 <- "Smit here"
+	} ()
+
+	for i := range 2 {
+		fmt.Println("Waiting for messages from channels...", i)
+		select {
+		case num := <-chan1:
+			fmt.Println("Received from chan1:", num) // Receive a number from chan1
+		case message := <-chan2:
+			   fmt.Println("Received from chan2:", message) // Receive a message from chan2
+		}
+	}
+	done := make(chan bool)
+
+	// go task(done)
+
+	emailChan := make(chan string, 100)
+	go sendEmail(emailChan, done) // Start a goroutine to send emails
+
+	for i :=range 5 {
+		emailChan <- fmt.Sprintf("%d@gmail.com", i) // Send emails to the channel
+	}
+	fmt.Println("Done sending emails")
+	close(emailChan) // Close the channel to signal that no more emails will be sent
+	<-done // Wait for the task to complete
+
 	/* messageChan <- "Hello, Channel!" // Send a message to the channel
 	var message string = <- messageChan
 
